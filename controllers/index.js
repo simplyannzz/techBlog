90630edbbde5e81f7057591c6daee3c3c082a620
Annie <anniebui0110@gmil.com>
2023-09-23 21:04:20 -0500

    controller index

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
+
+// Set up routes
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+// Export the router
+module.exports = router;
